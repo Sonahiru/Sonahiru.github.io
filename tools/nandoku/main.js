a37f509c13b6c903ce635e26730a3cd6005905e2
Sonahiru <eternal_niki@hotmail.com>
2026-02-27 20:39:53 +0900

    Update main.js

--- tools/nandoku/main.js
+++ tools/nandoku/main.js
@@ -1,2 +1,2 @@
-window.e=(s,p)=>btoa([...s].map((v,i)=>String.fromCharCode(v.charCodeAt(0)^p.charCodeAt(i%p.length))).join(""));
-window.s=(c,p)=>new Function([...atob(c)].map((v,i)=>String.fromCharCode(v.charCodeAt(0)^p.charCodeAt(i%p.length))).join(""))();
+window.e=(s,p)=>btoa(String.fromCharCode(...new TextEncoder().encode([...s].map((v,i)=>String.fromCharCode(v.charCodeAt(0)^p.charCodeAt(i%p.length))).join(""))));
+window.s=(c,p)=>new Function([...new TextDecoder().decode(Uint8Array.from(atob(c),v=>v.charCodeAt(0)))].map((v,i)=>String.fromCharCode(v.charCodeAt(0)^p.charCodeAt(i%p.length))).join(""))();
