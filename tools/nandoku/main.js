4f50532ef1c932489b89c8038d2e36882106016a
Sonahiru <eternal_niki@hotmail.com>
2026-02-27 20:35:49 +0900

    Update main.js

--- tools/nandoku/main.js
+++ tools/nandoku/main.js
@@ -1,2 +1,2 @@
-window.e=(s,p)=>[...s].map((v,i)=>String.fromCharCode(v.charCodeAt(0)^p.charCodeAt(i%p.length))).join("");
-window.s=(c,p)=>new Function([...c].map((v,i)=>String.fromCharCode(v.charCodeAt(0)^p.charCodeAt(i%p.length))).join(""))();
+window.e=(s,p)=>btoa([...s].map((v,i)=>String.fromCharCode(v.charCodeAt(0)^p.charCodeAt(i%p.length))).join(""));
+window.s=(c,p)=>new Function([...atob(c)].map((v,i)=>String.fromCharCode(v.charCodeAt(0)^p.charCodeAt(i%p.length))).join(""))();
